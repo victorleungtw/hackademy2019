bf430feeacc2e0bc32bf79bc1582e72bd18f0154
Victor Leung <victorleungtw@gmail.com>
2019-07-30 13:52:19 +0800

    Refractor rewards

--- src/app/rewards/rewards.page.ts
+++ src/app/rewards/rewards.page.ts
@@ -12,4 +12,21 @@ export class RewardsPage implements OnInit {
   ngOnInit() {
   }
 
+  items = [
+    {
+      name: 'Universal Travel Adapter',
+      points: 'Only 30 loyalty points',
+      img: '/assets/adapter.jpg'
+    },
+    {
+      name: 'Travel Backpack',
+      points: 'Only 100 loyalty points',
+      img: '/assets/backpack.jpg'
+    },
+    {
+      name: 'Travel Sleep Pillow',
+      points: 'Only 200 loyalty points',
+      img: '/assets/pillow.jpg'
+    }
+  ]
 }
