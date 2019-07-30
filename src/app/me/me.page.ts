f989475afb81021001978faa02b86170b8b00d96
Victor Leung <victorleungtw@gmail.com>
2019-07-30 13:48:29 +0800

    Refractor news

--- src/app/me/me.page.ts
+++ src/app/me/me.page.ts
@@ -12,4 +12,18 @@ export class MePage implements OnInit {
   ngOnInit() {
   }
 
+  news = [
+    {
+      subtitle: 'News about FX in your watch list',
+      title: 'EUR/USD consolidates in mid-range after the ECB, ahead of US GDP',
+      img: '/assets/madison.jpg',
+      content: 'EUR/USD is trading around 1.1150, in the middle of the broad range that characterized after the ECB left policy unchanged but hinted upcoming stimulus. The focus now shifts to the US GDP which is projected to show a slowdown.'
+    },
+    {
+      subtitle: 'News about FX in your watch list',
+      title: 'All eyes on US GDP after the ECB sends EUR/USD for a wild ride',
+      img: '/assets/money.jpg',
+      content: 'EUR/USD has stabilized around 1.1150 after hitting a new two-year low of 1.1101 after the European Central Bank opened the door to cutting rates and QE but refrained from imminent action.'
+    }
+  ]
 }
